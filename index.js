deada6f0f9a7d58bdd19d510151a3073fee789b0
Ferry Kobus <ferry@dtcmedia.nl>
2016-06-16 20:13:52 +0200

    two tracks

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Gpio = require('onoff').Gpio,
 	track_1 = new Gpio(4, 'out'),
+	track_2 = new Gpio(17, 'out'),
 	laps = 50,
 	off_interval = 1000,
 	on_interval = 10;
@@ -24,6 +25,7 @@ function run() {
 	var what = (laps === 0 || !!(laps && !(laps%2))) ? 1 : 0,
 		the_interval = ((what == 1) ? on_interval : off_interval);
 	track_1.writeSync(what);
+	track_2.writeSync(what);
 	console.log('running', laps, what);
 	if (laps > 0) {
 		laps--;
