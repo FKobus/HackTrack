462a37f4bbdfb0798933b42a148bbc58a8a5acca
Ferry Kobus <ferry@dtcmedia.nl>
2016-06-16 19:17:16 +0200

    its working

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var Gpio = require('onoff').Gpio,
 	track_1 = new Gpio(4, 'out'),
-	i = 10;
+	i = 1000;
 
 while(i > 0) {
-	track_1.writeSync(1);
+	track_1.writeSync((n === 0 || !!(n && !(n%2))) ? 1 : 0);	
 	i--;
 }
+track_1.writeSync(0);
