667a87e84384b8d1e42a32a279b673a88a65361c
Ferry Kobus <ferry@dtcmedia.nl>
2016-06-16 19:54:03 +0200

    did we fix it?

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Gpio = require('onoff').Gpio,
 	track_1 = new Gpio(4, 'out'),
-	i = 20,
+	laps = 50,
 	off_interval = 1000,
 	on_interval = 10;
 
@@ -8,27 +8,28 @@ console.log('OK HOI!');
 
 process.argv.forEach(function (val, index, array) {
   if (index == 2 && val != '') {
-  	off_interval = val;
+  	laps = val;
   }
   if (index == 3 && val != '') {
   	on_interval = val;
+  }
+  if (index == 4 && val != '') {
+  	off_interval = val;
   }
 });
 
 run();
 
-
 function run() {
-	var what = (i === 0 || !!(i && !(i%2))) ? 1 : 0;
+	var what = (i === 0 || !!(i && !(i%2))) ? 1 : 0,
+		the_interval = ((what == 1) ? on_interval : off_interval);
 	track_1.writeSync(what);
 	console.log('running', i, what);
-
 	if (i > 0) {
 		i--;
-		setTimeout(run, ((what == 1) ? on_interval : off_interval));
+		setTimeout(run, the_interval);
 	} else {
 		track_1.writeSync(0);
 		console.log('OK DOEI!');
 	}
 }
-
