1d1b8f02045b14b527352f0fd86479c4a7fc4bbe
Ferry Kobus <ferry@dtcmedia.nl>
2016-06-16 19:20:12 +0200

    timer added

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ var Gpio = require('onoff').Gpio,
 	track_1 = new Gpio(4, 'out'),
 	i = 1000;
 
-while(i > 0) {
-	track_1.writeSync((i === 0 || !!(i && !(i%2))) ? 1 : 0);	
-	i--;
+setTimeout(run, 10);
+
+
+function run() {
+	track_1.writeSync((i === 0 || !!(i && !(i%2))) ? 1 : 0);
+
+	if (i > 0) {
+		i--;
+		setTimeout(run, 10);
+	} else {
+		track_1.writeSync(0);
+		console.log('OK DOEI!');
+	}
 }
-track_1.writeSync(0);
+
