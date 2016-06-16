76a69b947ea468b1f67ce4ace2c149726b3ef46c
Ferry Kobus <ferry@dtcmedia.nl>
2016-06-16 19:39:45 +0200

    did we fix it?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Gpio = require('onoff').Gpio,
 	track_1 = new Gpio(4, 'out'),
 	i = 20,
-	interval = 20;
+	interval = 1000;
 
 console.log('OK HOI!');
 
@@ -16,15 +16,16 @@ setTimeout(run, interval);
 
 
 function run() {
-	console.log(interval);
-	var what = (i === 0 || !!(i && !(i%2))) ? 1 : 0,
-		new_interval = (interval/(what*1000));
+	var what = (i === 0 || !!(i && !(i%2))) ? 1 : 0;
 	track_1.writeSync(what);
-	console.log(what, new_interval);
 
 	if (i > 0) {
 		i--;
-		setTimeout(run, new_interval);
+		if (what == 1) {
+			setTimeout(run, interval/interval);
+		} else {
+			setTimeout(run, interval);
+		}
 	} else {
 		track_1.writeSync(0);
 		console.log('OK DOEI!');
