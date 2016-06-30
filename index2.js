d9007ec358c8ee765417667f9995d838d86c1067
Ferry Kobus <ferry@dtcmedia.nl>
2016-06-30 20:13:16 +0200

    its working

--- index2.js
+++ index2.js
@@ -21,6 +21,11 @@ board.on('ready', function() {
     }
   });
 
-  motor1.forward(10);
-  motor2.forward(10);
+  motor1.forward(100);
+  motor2.forward(100);
+  board.wait(2000, function() {
+    motor1.stop();
+    motor2.stop();
+
+  });
 });
