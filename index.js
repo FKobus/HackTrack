7b6ac7a125188eb8776ce7201dc9cf27c73abc0f
Ferry Kobus <ferry@dtcmedia.nl>
2016-08-03 19:27:48 +0200

    tracks assigned to accepted stories :D

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var raspi = require('raspi-io'),
 	board = new five.Board({
 		io: new raspi()
 	}),
-	http = require('http');
+	http = require('http'),
+	interval = 5000;
 
 board.on('ready', function() {
-	var motor1 = new five.Motor({pins:{pwm:26,dir:21}}),
-		motor2 = new five.Motor({pins:{pwm:23,dir:22}});
+	var internet_gekkies = new five.Motor({pins:{pwm:26,dir:21}}),
+		unexpectables = new five.Motor({pins:{pwm:23,dir:22}});
 
 	http.createServer(function(request, response) {
 		var headers = request.headers,
@@ -23,15 +24,23 @@ board.on('ready', function() {
 			// At this point, we have the headers, method, url and body, and can now
 			// do whatever we need to in order to respond to this request.
 			body = Buffer.concat(body).toString();
-			console.log(body);
+			var msg = JSON.parse(body);
+			if (msg.highlight == 'accepted') {
+				var project_id = parseInt(msg.project);
+				if (project_id == 1435890) {
+					internet_gekkies.forward(120);
+					board.wait(interval, function() {
+						internet_gekkies.stop();
+					});
+				} else if (project_id == 1547353) {
+					unexpectables.forward(120);
+					board.wait(interval, function() {
+						unexpectables.stop();
+					});
+				}
+			}
+			response.writeHead(200, {'Content-Type': 'application/json'});
+			response.end();
 		});
-		console.log("Server listening on: http://localhost:80");
 	}).listen(80);
-
-	// motor1.forward(120);
-	// //motor2.forward(120);
-	// board.wait(10000, function() {
-	// 	motor1.stop();
-	// 	//motor2.stop();
-	// });
 });
