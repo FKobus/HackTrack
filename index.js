f47270b5b4c64f410ac8797bf4d8a400e7972672
Ferry Kobus <ferry@dtcmedia.nl>
2016-08-03 18:37:29 +0200

    Update to v2

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
-var Gpio = require('onoff').Gpio,
-	track_1 = new Gpio(4, 'out'),
-	track_2 = new Gpio(17, 'out'),
-	laps = 50,
-	off_interval = 1000,
-	on_interval = 10;
-
-console.log('OK HOI!');
-
-process.argv.forEach(function (val, index, array) {
-  if (index == 2 && val != '') {
-  	laps = val;
-  }
-  if (index == 3 && val != '') {
-  	on_interval = val;
-  }
-  if (index == 4 && val != '') {
-  	off_interval = val;
-  }
+var raspi = require('raspi-io');
+var five = require('johnny-five');
+var board = new five.Board({
+  io: new raspi()
 });
 
-run();
+board.on('ready', function() {
+  var motor1 = new five.Motor({
+    pins: {
+      pwm: 26,
+      dir: 21
+    }
+  });
 
-function run() {
-	var what = (laps === 0 || !!(laps && !(laps%2))) ? 1 : 0,
-		the_interval = ((what == 1) ? on_interval : off_interval);
-	track_1.writeSync(what);
-	track_2.writeSync(what);
-	console.log('running', laps, what);
-	if (laps > 0) {
-		laps--;
-		setTimeout(run, the_interval);
-	} else {
-		track_1.writeSync(0);
-		track_2.writeSync(0);
-		console.log('OK DOEI!');
-	}
-}
+  var motor2 = new five.Motor({
+    pins: {
+      pwm: 23,
+      dir: 22
+    }
+  });
+
+  motor1.forward(120);
+  //motor2.forward(120);
+  board.wait(10000, function() {
+    motor1.stop();
+    //motor2.stop();
+  });
+});
