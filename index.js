b93b20e9bb1eb453e2c6d40567861c1871079699
Ferry Kobus <ferry@dtcmedia.nl>
2016-09-09 17:38:15 +0200

    added delay

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var board = new five.Board({
 var http = require('http');
 var interval = 6000;
 var speed = 134;
-var offset = 12;
+var offset = 16;
 
 board.on('ready', function() {
 	var internet_gekkies = new five.Motor({pins:{pwm:26,dir:21}});
